8ad0f0ead15cc6f2fe7470a3cc446e6da63f9145
KevinR08 <79124824+KevinR08@users.noreply.github.com>
2022-11-12 15:45:30 -0500

    Prueba Commit Kevin2

--- src/Logica/Juez.java
+++ src/Logica/Juez.java
@@ -1,4 +1,10 @@
 package Logica;
 
 public class Juez extends Participante{
+    private String rolJuez;
+
+    public Juez(int idparticipante, String nombres, String nickname, double sociedadDebate, String rolJuez) {
+        super(idparticipante, nombres, nickname, sociedadDebate);
+        this.rolJuez = rolJuez;
+    }
 }
---
+++ src/Logica/Mocion.java
@@ -0,0 +1,21 @@
+package Logica;
+
+public class Mocion {
+    private int id;
+    private String descripcion;
+    private String tipo;
+
+    public Mocion(int id, String descripcion, String tipo) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Mocion --> " +
+                "\tID:" + id +
+                "\tDescripción:" + descripcion  +
+                "\tTipo=" + tipo;
+    }
+}
