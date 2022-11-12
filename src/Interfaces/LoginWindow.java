1be4246fb1b6c0e5ab27abe1a246087d2beac289
CristianSoft <cristian.verduga@epn.edu.ec>
2022-11-12 01:43:41 -0500

    Primer Commit Cristian

---
+++ src/Interfaces/LoginWindow.java
@@ -0,0 +1,35 @@
+package Interfaces;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginWindow extends JDialog {
+    private JPanel contentPane;
+    private JTextField usuarioTextField;
+    private JTextField contraseñaTextField;
+    private JButton loginButton;
+
+    public LoginWindow() {
+        setContentPane(contentPane);
+        setModal(true);
+        setResizable(false);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+
+
+
+    /*public static void main(String[] args) {
+        LoginWindow dialog = new LoginWindow();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+     */
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import Interfaces.LoginWindow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        star();
+    }
+
+    public static void star(){
+        LoginWindow dialog = new LoginWindow();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
     }
 }
