45f7709b4cf3bcf97df9ab45f9d607be649befe5
KevinR08 <79124824+KevinR08@users.noreply.github.com>
2022-11-12 19:27:03 -0500

    Commit Kev - Change Debate Class

--- src/Logica/Debate.java
+++ src/Logica/Debate.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Debate {
     private int idDebate;
-    private Juez juezPrincipal;
-    private ArrayList panelistas = new ArrayList<Juez>();
+    private Participante juezPrincipal;
+    private ArrayList panelistas = new ArrayList<Participante>();
     private Mocion mocion;
-    private ArrayList CAO = new ArrayList<Debatiente>();
-    private ArrayList CAG = new ArrayList<Debatiente>();
-    private ArrayList CBG = new ArrayList<Debatiente>();
-    private ArrayList CBO = new ArrayList<Debatiente>();
+    private ArrayList CAO = new ArrayList<Participante>();
+    private ArrayList CAG = new ArrayList<Participante>();
+    private ArrayList CBG = new ArrayList<Participante>();
+    private ArrayList CBO = new ArrayList<Participante>();
 
-    public Debate(int idDebate, Juez juezPrincipal, ArrayList panelistas, Mocion mocion, ArrayList CAO, ArrayList CAG, ArrayList CBG, ArrayList CBO) {
+    public Debate(int idDebate, Participante juezPrincipal, ArrayList panelistas, Mocion mocion, ArrayList CAO, ArrayList CAG, ArrayList CBG, ArrayList CBO) {
         this.idDebate = idDebate;
         this.juezPrincipal = juezPrincipal;
         this.panelistas = panelistas;
